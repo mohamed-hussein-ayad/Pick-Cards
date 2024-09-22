1e69553bc9de909f0b2a812538e166760e8be504
mohamed ayad <126675322+mohamed-hussein-ayad@users.noreply.github.com>
2024-09-22 11:05:34 +0300

    fix the bug in modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,35 +1,23 @@
-import { useEffect } from "react";
+import { useRef, useEffect } from "react";
+import { createPortal } from "react-dom";
 
-import ProgressBar from "./ProgressBar.jsx";
+function Modal({ open, children, onClose }) {
+  const dialog = useRef();
 
-const TIMER = 3000;
-
-export default function DeleteConfirmation({ onConfirm, onCancel }) {
   useEffect(() => {
-    console.log("TIMER SET");
-    const timer = setTimeout(() => {
-      onConfirm();
-    }, TIMER);
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
 
-    return () => {
-      console.log("Cleaning up timer");
-      clearTimeout(timer);
-    };
-  }, [onConfirm]);
-
-  return (
-    <div id="delete-confirmation">
-      <h2>Are you sure?</h2>
-      <p>Do you really want to remove this place?</p>
-      <div id="confirmation-actions">
-        <button onClick={onCancel} className="button-text">
-          No
-        </button>
-        <button onClick={onConfirm} className="button">
-          Yes
-        </button>
-      </div>
-      <ProgressBar timer={TIMER} />
-    </div>
+  return createPortal(
+    <dialog className="modal" ref={dialog} onClose={onClose}>
+      {open ? children : null}
+    </dialog>,
+    document.getElementById("modal")
   );
 }
+
+export default Modal;
